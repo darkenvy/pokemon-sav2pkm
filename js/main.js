f8fbaa6eb37da46ad7cc935c7c6bd9b23a3e2668
darkenvy <darkenvy6@gmail.com>
2016-08-20 10:28:58 -0700

    Cleaned up prototype code

--- js/main.js
+++ js/main.js
@@ -1,27 +1,51 @@
+// Wait for Dom to be ready before listening for file loadings.
 document.addEventListener("DOMContentLoaded", function() {
-  function onfilechange(evt) {
+  document.getElementById('file').addEventListener('change', poketool.onfilechange);
+});
+
+// Primary Namespace
+var poketool = {
+  onfilechange: function(evt) {
     var selFile = evt.target.files[0];
     var reader = new FileReader();
     reader.readAsArrayBuffer(selFile);
+    // Callback because file reading is async
     reader.onloadend = function (e) {
-        if (e.total !== 65536) {console.log('Filesize not correct', e.total); return 1;}
-
-        // Load the file into an array of 16 blocks (indicies)
-        var loadedFile = new Int8Array(e.target.result);
+      if (e.total !== 65536) {console.log('Filesize not correct', e.total); return 1;}
+      // declare temp function for cleanliness
+      var get16Chunks = function() {
         var file = [];
+        var loadedFile = new Int8Array(e.target.result);
         for (var i=0; i<16; i++) {
           var tmp = new Int8Array(loadedFile.slice( i*4096, (i+1)*4096 ))
           file.push(tmp);
         }
-        var loadedFile = undefined; //unload loadedFile gracefully
-
-        // Check for block type
-        for (var i=0; i<16; i++) {
-          console.log(file[i][4084])
-        }
-
-
+        return file;
+      }
+      var file = get16Chunks();
+      main(file); // Start main now that a file is loaded
     };
+  },
+  printBox1: function(file) {
+    if (file[1][4084] === 5 ) console.log('yes it is');
   }
-  document.getElementById('file').addEventListener('change', onfilechange);
-})
+}
+
+// ---------------------------------- //
+//               Main                 //
+// ---------------------------------- //
+// main only starts after a file is loaded
+// parameter 'file' is the savedata that is split into 16 chunks
+function main(file) {
+  poketool.printBox1(file);
+}
+
+
+
+
+
+
+// Check for block type
+// for (var i=0; i<16; i++) {
+//   console.log(file[i][4084])
+// }

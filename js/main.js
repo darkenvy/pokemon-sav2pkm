b9050c590c127591bcffda95741d91fcda82a987
darkenvy <darkenvy6@gmail.com>
2016-08-20 21:10:28 -0700

    expanded namespace

--- js/main.js
+++ js/main.js
@@ -29,6 +29,20 @@ var poketool = {
   mergeArrays: function(arr1, arr2) {
    return arr1 + arr2.join();
   },
+  bin2Str: function(len7Str) {
+    // Convert binary to string
+
+    // should change len7Str now that this is a more universal function
+    // Requires the input to be only 7 chars in length
+    // However, this function will return the whoel boxname
+    var output = '';
+    len7Str.forEach(function(letter) {
+      if (letter > -96 && letter < -85) letter = letter + 143;
+      else letter = letter + 134;
+      output += String.fromCharCode(letter);
+    })
+    return output;
+  },
   compilePCBox: function(file) {
     var allBox = [];
     // Check for block type
@@ -41,17 +55,17 @@ var poketool = {
     }
     return new Int8Array(allBox.split(','));
   },
-  bin2Name: function(len7Str) {
-    // should change len7Str now that this is a more universal function
-    // Requires the input to be only 7 chars in length
-    // However, this function will return the whoel boxname
-    var output = '';
-    len7Str.forEach(function(letter) {
-      if (letter > -96 && letter < -85) letter = letter + 143;
-      else letter = letter + 134;
-      output += String.fromCharCode(letter);
-    })
-    return output;
+  getBoxName: function(entirePCBox, boxNum) {
+    var nameList = entirePCBox.slice(33604,33730); // All box names
+    var boxName = nameList.slice(boxNum*9, (boxNum*9)+7); // one box name
+    return this.bin2Str(boxName);
+  },
+  
+  setPkm: function(pkm, slot) {
+    // Returns a new bank of 420 slots.
+    // slot is a number 0-420. 
+    // pkm is the pokemon 80b data
+
   }
 }
 
@@ -63,22 +77,26 @@ var poketool = {
 var pcBox;
 var boxNames;
 function main(file) {
+  // global scope for now. for debugging
   pcBox = poketool.compilePCBox(file);
-  boxNames = pcBox.slice(33604,33730)
-  for (var i=0; i<8; i++) {
-    var section = boxNames.slice(i*9,(i*9)+7)
-    console.log(poketool.bin2Name(section));
-  }
+  console.log(poketool.getBoxName(pcBox, 1));
+  // boxNames = pcBox.slice(33604,33730);
+  
+  // Display all box names
+  // for (var i=0; i<8; i++) {
+  //   var section = boxNames.slice(i*9,(i*9)+7)
+  //   console.log(poketool.bin2Str(section));
+  // }
 
-  console.log(poketool.bin2Name( boxNames.slice(0,7) ));
-  var box1 = pcBox.slice(4,33604);
-  console.log(box1.length);
-  for (var i=0; i<30; i++) {
-    var pkm = box1.slice(i*80,(i*80)+80);
-    var pkmName = pkm.slice(8,18);
-    console.log( poketool.bin2Name(pkmName) )
-  }
-  // console.log(boxNames, boxNames.length);
+  // Display all pokemon
+  // var allBoxes = pcBox.slice(4,33604);
+  // for (var i=0; i<420; i++) {
+  //   var pkm = allBoxes.slice(i*80,(i*80)+80);
+  //   var pkmName = pkm.slice(8,18);
+  //   console.log( poketool.bin2Str(pkmName) )
+  // }
+
+
 }
 
 

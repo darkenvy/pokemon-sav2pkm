3091f34483595e44205221e6c771239fe4c5d6cd
darkenvy <darkenvy6@gmail.com>
2017-01-11 13:13:32 -0800

    Initiated Uint8Arrays for data

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Callback because file reading is async
     reader.onloadend = function (e) {
-      if (e.total !== 65536) {console.log('Filesize not correct', e.total); return 1;}
+      if (e.total !== 65536 && e.total !== 131072) {console.log('Filesize not correct', e.total); return 1;}
       else {
         // console.log('loaded: ', e.target.result)
         fileLoaded(e.target.result);
@@ -30,18 +30,31 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function fileLoaded(arrayBuffer) {
   // console.log(new Uint8Array(arrayBuffer));
-  var saveslot = new SaveSlot(arrayBuffer);
-  console.log(saveslot);
+  var saveSlotA = new SaveSlot(arrayBuffer, 0);
+  var saveSlotB = new SaveSlot(arrayBuffer, 1);
+
+  console.log(saveSlotA);
+  console.log(saveSlotB);
+
+  // Will need to compare SlotA & B to see which is newer
+
+
 }
 
 // ----------------------------------------------------------------- //
 
-function SaveSlot(arrayBuffer) {
-  this.sections = [];
 
+// SaveSlot Object. up to 2 saveslots per game save.
+function SaveSlot(arrayBuffer, slotInt) {
+  this.sections = [];
+  this.pcBuffer;
+
+  // Prevents error if save is only one SaveSlot big
+  if (slotInt === 1 && arrayBuffer.byteLength === 65536) return;
+  
+  // Generate Section objects and append to SaveSlot
   var arrayBufferTyped = new Uint8Array(arrayBuffer)
-  for (var i=0; i<16; i++) {
-    // this.sections.push( arrayBufferTyped.slice(i*4096, (i+1)*4096) )
+  for (var i=0+(slotInt*16); i<16+(slotInt*16); i++) { // 0-16 or 16-32
     var sectionSlice = arrayBufferTyped.slice(i*4096, (i+1)*4096)
     this.sections.push( new Section(sectionSlice) )
   }
@@ -49,20 +62,31 @@ function SaveSlot(arrayBuffer) {
 }
 SaveSlot.prototype = {
   constructor: SaveSlot
-}
+};
 
 
 
-
+// There are 16 sections per SaveSlot. All 16 belong to respective SaveSlot instance
 function Section(slice) {
-  this.data = slice
+  this.data = slice;
+  this.id = this.data[4096-12]; // Technically 2Bytes But second Byte never used
+  this.checksum = this.data.slice(4096-10, 4096-8);
+  this.validationCode = this.data.slice(4096-8, 4096-4); // Always 37,32,1,8
+  this.saveIndex = this.data.slice(4096-4, 4096);
 }
-Section.prototype = new SaveSlot()
-Section.prototype.constructor = Section
+Section.prototype = new SaveSlot();
+Section.prototype.constructor = Section;
 
 
 
+// One PCBuffer per SaveSlot. A PCBuffer contains all in-game PC boxes
+function PCBuffer = {
 
+}
+Section.prototype = new SaveSlot();
+Section.prototype.constructor = PCBuffer;
+
+// 14 in-game PC boxes. All Boxs belong to PCBuffer.
 // function Box() {
 
 // }
